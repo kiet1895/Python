6876d9908581588df52ff2488f930e282ac80d40
c++ <kiet1895@gmail.com>
2021-05-23 16:21:20 +0700

    hocj python

---
+++ chivihinhtron.py
@@ -0,0 +1,6 @@
+import math
+R=float (input("nhap R: "))
+cv=R*2* math.pi
+dt=R**2 * math.pi
+print(round(cv,1))
+print(round(dt,1))
---
+++ test chuoi.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+s= 'Chào %s, chuc ban mot ngay tot lanh.'
+print(s %("An")
