e9638bdd36cbf99d8b386807d5d630425a8eac16
c++ <kiet1895@gmail.com>
2021-05-28 09:17:15 +0700

    Tim so chan in ra thanh chuoi

--- NhapChuoiSXABC.py
+++ NhapChuoiSXABC.py
@@ -1,5 +1,3 @@
-from calendar import c
-from posixpath import split
 
 
 # item=[ x for x in input("nhap chu can sap xep:").split(',')]
---
+++ TimSoChan.py
@@ -0,0 +1,5 @@
+value=[]
+for i in range(1000,3001,2):
+    s= str(i)
+    value.append(s)
+print((',').join(value))
